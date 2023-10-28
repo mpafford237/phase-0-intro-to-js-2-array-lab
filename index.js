c55ad76121f59d5597de60a81e51a4a742e70d5c
Mason Pafford <mdpafford2015@gmail.com>
2023-10-28 18:07:02 -0500

    Completed Array Assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+function appendCat(name) {
+    const copyOfCats = [
+        ...cats.slice(), 
+        name
+    ];
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = [
+        name, 
+        ...cats.slice()
+    ];
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    const copyOfCats = [...cats.slice(0, -1)];
+    return copyOfCats
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = [...cats.slice(1)];
+    return copyOfCats
+}
